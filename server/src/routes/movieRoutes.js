a9e94e23470875a2ca8853487c0ab57888426023
Akash S <akashspalloor@gmail.com>
2025-03-08 16:53:47 +0530

    added review routes inside movie routes

--- server/src/routes/movieRoutes.js
+++ server/src/routes/movieRoutes.js
@@ -2,12 +2,19 @@ import express from "express";
 import checkAuth from "../middlewares/checkAuth.js";
 import checkAdmin from "../middlewares/checkAdmin.js";
 import { addMovie, deleteMovie, getAllMovies, getMovieDetails } from "../controllers/MovieController.js";
+import { addReview, getAllReviews } from "../controllers/reviewController.js";
 const router = express.Router();
 
 
+// -----------Movie routes-----------
 router.post('/add-movie', checkAdmin, addMovie)
 router.delete('/delete-movie/:id', checkAdmin, deleteMovie)
 router.get('/movies',getAllMovies)
 router.get('/movie-details/:id', checkAuth, getMovieDetails)
+
+
+// -----------Review routes-----------
+router.post('/:movieId/add-review', checkAuth, addReview)
+router.get('/:movieId/getall-reviews', checkAuth, getAllReviews)
 
 export default router
