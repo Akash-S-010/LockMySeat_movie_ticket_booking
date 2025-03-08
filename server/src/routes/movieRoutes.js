015982dc2a9f84d856d0429f6aa1e7c8385b368e
Akash S <akashspalloor@gmail.com>
2025-03-08 20:56:12 +0530

    access for both admin and owner

--- server/src/routes/movieRoutes.js
+++ server/src/routes/movieRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import checkAuth from "../middlewares/checkAuth.js";
 import checkAdmin from "../middlewares/checkAdmin.js";
+import checkOwner from "../middlewares/checkOwner.js";
 import { addMovie, deleteMovie, getAllMovies, getMovieDetails } from "../controllers/MovieController.js";
 import { addReview, getAllReviews } from "../controllers/reviewController.js";
 const router = express.Router();
@@ -11,6 +12,7 @@ router.post('/add-movie', checkAdmin, addMovie)
 router.delete('/delete-movie/:id', checkAdmin, deleteMovie)
 router.get('/movies',getAllMovies)
 router.get('/movie-details/:id', checkAuth, getMovieDetails)
+router.get('/total-movies',checkAdmin,checkOwner,totalMovies)
 
 
 // -----------Review routes-----------
