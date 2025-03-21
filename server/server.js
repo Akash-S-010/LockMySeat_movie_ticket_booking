ed1c31c521f669ad65ba0fd508d900ca74e1452c
Akash S <akashspalloor@gmail.com>
2025-03-21 16:24:42 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import cookieParser from "cookie-parser";
 import apiRoutes from "./src/routes/index.js";
+import cors from "cors"
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 
 app.use('/api', apiRoutes)
