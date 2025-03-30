ee33c45b636ef3b32534aedc711fc1ccca0be051
Akash S <akashspalloor@gmail.com>
2025-03-30 16:10:52 +0530

    fixed client domain issue (/)

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173","https://lock-my-seat.vercel.app/"],
+    origin: ["http://localhost:5173","https://lock-my-seat.vercel.app"],
     credentials: true
 }))
 
