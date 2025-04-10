63f6a148c3af307cd32f9f69e5850d7b33af0991
Akash S <akashspalloor@gmail.com>
2025-04-10 13:01:24 +0530

    z-index fixed

--- client/src/components/ui/Banner.jsx
+++ client/src/components/ui/Banner.jsx
@@ -71,7 +71,7 @@ const BannerSlider = () => {
       ))}
 
       {/* Navigation Dots */}
-      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex space-x-2 z-30">
+      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex space-x-2 z-10">
         {slides.map((_, index) => (
           <button
             key={index}
