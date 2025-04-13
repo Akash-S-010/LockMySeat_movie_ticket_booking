fc469b9590df59b98f56489e8974fc0e2b483ce5
Akash S <akashspalloor@gmail.com>
2025-04-13 10:42:20 +0530

    animation updated

--- client/src/pages/user/PaymentSuccess.jsx
+++ client/src/pages/user/PaymentSuccess.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import { motion } from "framer-motion";
-import successAnimation from "../../assets/animationFiles/Animation - 1744434301480.json";
+import successAnimation from "../../assets/animationFiles/Animation - 1744520855804.json";
 
 const PaymentSuccess = () => {
   const navigate = useNavigate();
